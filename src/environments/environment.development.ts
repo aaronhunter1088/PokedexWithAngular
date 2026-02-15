c0d92f617279701d2ef96ca3847585d669a8e689
Michael Ball <aaronhunter@live.com>
2026-02-15 13:51:22 -0600

    update dev env for mobile functionality

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: false,
-    landingPageUrl: 'http://localhost:4200'
+    get landingPageUrl(): string {
+        // Check if running on mobile device
+        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        return isMobile ? 'http://192.168.1.152:4200' : 'http://localhost:4200';
+    }
 };
