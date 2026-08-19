5d11747e2546e01ffa788f823e5eb3841c0dc388
Michael Ball <aaronhunter@live.com>
2026-08-18 19:12:15 -0500

    default coloring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {PokedexComponent} from './pokedex/pokedex.component';
 import {NgxPaginationModule} from 'ngx-pagination';
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {ArraySortPipe} from './array-sort.pipe';
 import {EvolvesHowComponent} from './evolves-how/evolves-how.component';
